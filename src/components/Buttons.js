262261adab99e3fb7c0d772c861e7203f951d5fe
Zack Lukas <zacklukas@Zacks-MacBook-Pro.local>
2023-08-18 00:00:36 -0500

    updated the sat

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,16 @@
-const Buttons = () => {
+import React from "react";
+import satData from "./satData";
+
+const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      {displaySats.map((sat, id) => (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      ))}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+    </div>
   );
 };
 
